0de6dbd74fd526ed2b457455b2adbe7beeb72465
Blitzdude <25263772+Blitzdude@users.noreply.github.com>
2025-05-04 14:42:26 +0300

    day 1 part 1 done

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,4 +1,5 @@
 import day1 from './index';
+import * as fs from 'fs'
 
 describe('On Day 1', () =>{
     it(`part1 is identity function`, ()=>{
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -6,12 +6,51 @@ class Day1 extends Day {
         super(1);
     }
     
+    
     solveForSample(input: string): string {
-        return input;
+        let pair1: Array<number> = []
+        let pair2: Array<number> = []
+
+        input.split("\n").forEach(line => {
+            let pair = line.split("  ");
+            pair1.push(parseInt(pair[0]));
+            pair2.push(parseInt(pair[1]));
+        });
+
+        pair1.sort();
+        pair2.sort();
+
+        let sum = 0;
+        for (let i = 0; i < pair1.length; i++ )
+        {
+            let pairDiff = pair1[i] - pair2[i] >= 0 ? pair1[i] - pair2[i] : -(pair1[i] - pair2[i]);
+            sum += pairDiff;
+        }
+
+        return sum.toString();
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        let pair1: Array<number> = []
+        let pair2: Array<number> = []
+
+        input.split("\n").forEach(line => {
+            let pair = line.split("  ");
+            pair1.push(parseInt(pair[0]));
+            pair2.push(parseInt(pair[1]));
+        });
+
+        pair1.sort();
+        pair2.sort();
+
+        let sum = 0;
+        for (let i = 0; i < pair1.length; i++ )
+        {
+            let pairDiff = pair1[i] - pair2[i] >= 0 ? pair1[i] - pair2[i] : -(pair1[i] - pair2[i]);
+            sum += pairDiff;
+        }
+
+        return sum.toString();
     }
 
     solveForPartTwo(input: string): string {
