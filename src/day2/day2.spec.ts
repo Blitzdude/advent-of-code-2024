d30ecdd8e65265157ed2e00da0c4ceb61ac083cf
Blitzdude <25263772+Blitzdude@users.noreply.github.com>
2025-05-10 20:32:26 +0300

    Added debugging. Created function to verify monotonality of functions.

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -7,16 +7,16 @@ describe('On Day 2', () =>{
 });
 
 describe('On Day 2', () =>{
-    it(`testLevels function should return true with safe input`, ()=>{
-        expect(day2.testLevels([1,2,3,4,5]));
+    it(`verifyLevelsAreStable function should return true with monotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([1,2,3,4,5])).toBe(true);
+        expect(day2.verifyLevelsAreMonotone([5,4,3,2,1])).toBe(true);
     })
 })
 
 describe('On Day 2', () =>{
-    it(`testLevels function should return false, with unsafe input`, ()=>{
-        expect(day2.testLevels([1,2,3,5,1]) == false);
-        expect(day2.testLevels([1,2,7,8,9]) == false);
-        expect(day2.testLevels([1,3,2,4,5]) == false);
-        expect(day2.testLevels([8,6,4,4,1]) == false);
+    it(`verifyLevelsAreStable function should return false, with unmonotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([1,1,2,2,3])).toBe(false);
+        expect(day2.verifyLevelsAreMonotone([1,2,1,2,1])).toBe(false);
+        expect(day2.verifyLevelsAreMonotone([3,2,1,2,3])).toBe(false);
     })
 })
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -18,8 +18,7 @@ class Day2 extends Day {
         let reports = input.split("\n");
         reports.forEach(report => {
             let levels = report.split(" ").map(Number);
-            let is_unsafe = this.testLevels(levels)
-            if (!is_unsafe)
+            if (this.verifyLevelsAreMonotone(levels))
                 safe_reports += 1;
         });
         
@@ -30,14 +29,43 @@ class Day2 extends Day {
         return input;
     }
 
-    testLevels(input: Array<number>): boolean {
+    verifyLevelsAreMonotone(input: Array<number>): boolean {
+        let is_monotone = true; // is the function always increasing/decreasing
+        let is_increasing = true; // true means increasing, false means decreasing
+
+        if(input[0] > input[1]) {
+            is_increasing = false;
+        }
+
+        for (let i = 0; i < input.length-1; i++) {
+            if (input[i] == input[i+1]) {
+                is_monotone = false;
+                break;
+            }
+            // next number is increasing, when levels should decrease
+            if (input[i] < input[i+1] && is_increasing == false) {
+                is_monotone = false;
+                break;
+            }
+            // next number is decreasing, when levels should increase
+            if (input[i] > input[i+1] && is_increasing == true) {
+                is_monotone = false;
+                break;
+            }
+        }
+        return is_monotone;
+    }
+
+    // return true if levels only increase or decrese
+    verifyLevelsAreStable(input: Array<number>): boolean {
         let is_safe = true;
         for (let i = 1; i < input.length; i++) {
             // rule 1: Levels must decrease or increase.
-            // rule 2: adjacent levels differ by at least 1 or at most 3
+            // rule 2: adjacent levels must differ by 1
+            // rule 3: adjacent levels must differ by 3 or less.
             
             // If absolute value is > 3 or values are same = Unsafe test
-            if (Math.abs(input[i-1] - input[i]) > 3 || input[i-1] == input[i])
+            if (Math.abs(input[i-1] - input[i]) >= 4 || input[i-1] - input[i] == 0)
             {
                 is_safe = false;
             }
@@ -52,7 +80,6 @@ class Day2 extends Day {
                 is_safe = false;
             }
         }
-        console.log(`Test is: ${is_safe ? "Safe" : "Unsafe"}`);
         return is_safe;
     }
 }
