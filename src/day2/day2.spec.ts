2acfacaa6c89f44168067aa87926aa8eda37404a
Blitzdude <25263772+Blitzdude@users.noreply.github.com>
2025-05-17 14:29:16 +0300

    Modified day 2 functions to return number values.

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -7,34 +7,32 @@ describe('On Day 2', () =>{
 });
 
 describe('On Day 2', () =>{
-    it(`verifyLevelsAreMonotone function should return true with monotone input`, ()=>{
-        expect(day2.verifyLevelsAreMonotone([7,6,4,2,1])).toBe(true);
-        expect(day2.verifyLevelsAreMonotone([1,3,6,7,9])).toBe(true);
-        expect(day2.verifyLevelsAreMonotone([1,2,4,5,6,7,9,11,13])).toBe(true);
+    it(`verifyLevelsAreMonotone function should return -1 with monotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([7,6,4,2,1])).toBe(-1);
+        expect(day2.verifyLevelsAreMonotone([1,3,6,7,9])).toBe(-1);
+        expect(day2.verifyLevelsAreMonotone([1,3,2,4,5])).toBe(-1);
     })
 })
 
 describe('On Day 2', () =>{
-    it(`verifyLevelsAreMonotone function should return false, with unmonotone input`, ()=>{
-        expect(day2.verifyLevelsAreMonotone([1,1,2,2,3,3,4,4])).toBe(false);
-        expect(day2.verifyLevelsAreMonotone([1,3,2,4,5])).toBe(false);
-        expect(day2.verifyLevelsAreMonotone([3,2,1,2,3])).toBe(false);
-        expect(day2.verifyLevelsAreMonotone([8,6,4,4,1])).toBe(false);
+    it(`verifyLevelsAreMonotone function should return 0 or more, with unmonotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([1,3,2,4,5])).toBe(1);
+        expect(day2.verifyLevelsAreMonotone([3,2,1,2,3])).toBe(2);
     })
 });
 
 describe('On Day 2', () => {
-    it(`verifyLevelsAreStable function should return true with stable input`, ()=>{
-        expect(day2.verifyLevelsAreStable([7,6,4,2,1])).toBe(true);
-        expect(day2.verifyLevelsAreStable([1,3,6,7,9])).toBe(true);
+    it(`verifyLevelsAreStable function should return -1 with stable input`, ()=>{
+        expect(day2.verifyLevelsAreStable([7,6,4,2,1])).toBe(-1);
+        expect(day2.verifyLevelsAreStable([1,3,6,7,9])).toBe(-1);
     })
 })
 
 describe('On Day 2', () => {
-    it(`verifyLevelsAreStable function should return false with unstable input`, ()=>{
-        expect(day2.verifyLevelsAreStable([1,2,7,8,9])).toBe(false);
-        expect(day2.verifyLevelsAreStable([9,7,6,2,1])).toBe(false);
-        expect(day2.verifyLevelsAreStable([12,11,9,9,6,5,4,3,2,1])).toBe(false);
-        expect(day2.verifyLevelsAreStable([11,9,8,6,5,4,3,2,2])).toBe(false);
+    it(`verifyLevelsAreStable function should return 0 or more with unstable input`, ()=>{
+        expect(day2.verifyLevelsAreStable([1,2,7,8,9])).toBe(1);
+        expect(day2.verifyLevelsAreStable([9,7,6,2,1])).toBe(2);
+        expect(day2.verifyLevelsAreStable([12,11,9,9,6,5,4,3,3,1])).toBe(2);
+        expect(day2.verifyLevelsAreStable([11,9,8,6,5,4,3,2,2])).toBe(7);
     })
 })
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -17,8 +17,8 @@ class Day2 extends Day {
         let reports = input.split("\n");
         reports.forEach(report => {
             let levels = report.split(" ").map(Number);
-            if (this.verifyLevelsAreMonotone(levels) &&
-                this.verifyLevelsAreStable(levels))
+            if (this.verifyLevelsAreMonotone(levels) == -1 &&
+                this.verifyLevelsAreStable(levels) == -1)
                 safe_reports += 1;
         });
         
@@ -29,9 +29,8 @@ class Day2 extends Day {
         return input;
     }
 
-    // returns true, if levels only increase/decrease.
-    verifyLevelsAreMonotone(input: Array<number>): boolean {
-        let is_monotone = true; // is the function always increasing/decreasing
+    // returns -1, if levels only increase/decrease.
+    verifyLevelsAreMonotone(input: Array<number>): number {
         let is_increasing = true; // true means increasing, false means decreasing
 
         if(input[0] > input[1]) {
@@ -40,35 +39,31 @@ class Day2 extends Day {
 
         for (let i = 0; i < input.length-1; i++) {
             if (input[i] == input[i+1]) {
-                is_monotone = false;
-                break;
+                return i;
             }
             // next number is increasing, when levels should decrease
             if (input[i] < input[i+1] && is_increasing == false) {
-                is_monotone = false;
-                break;
+                return i;
             }
             // next number is decreasing, when levels should increase
             if (input[i] > input[i+1] && is_increasing == true) {
-                is_monotone = false;
-                break;
+                return i;
             }
         }
-        return is_monotone;
+        return -1;
     }
 
-    // verify, that levels always change at least by 1 or 3
-    verifyLevelsAreStable(input: Array<number>): boolean {
-        let is_safe = true;
+    // verify, that levels always change at least by 1 or 3. Returns -1 if safe, otherwise returns index of the element in levels.
+    verifyLevelsAreStable(input: Array<number>): number {
+
         for (let i = 0; i <= input.length-1; i++) {
             // If absolute value is > 3 or values are same = Unsafe test
             if (Math.abs(input[i-1] - input[i]) >= 4 || input[i-1] - input[i] == 0)
             {
-                is_safe = false;
-                break;
+                return i;
             }
         }
-        return is_safe;
+        return -1;
     }
 }
 
