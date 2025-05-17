de308c88ed9e8f4c477c482fa36bb4e96df24e2a
Blitzdude <25263772+Blitzdude@users.noreply.github.com>
2025-05-17 12:55:51 +0300

    Day2 part 1 right answer.

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -7,16 +7,34 @@ describe('On Day 2', () =>{
 });
 
 describe('On Day 2', () =>{
-    it(`verifyLevelsAreStable function should return true with monotone input`, ()=>{
-        expect(day2.verifyLevelsAreMonotone([1,2,3,4,5])).toBe(true);
-        expect(day2.verifyLevelsAreMonotone([5,4,3,2,1])).toBe(true);
+    it(`verifyLevelsAreMonotone function should return true with monotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([7,6,4,2,1])).toBe(true);
+        expect(day2.verifyLevelsAreMonotone([1,3,6,7,9])).toBe(true);
+        expect(day2.verifyLevelsAreMonotone([1,2,4,5,6,7,9,11,13])).toBe(true);
     })
 })
 
 describe('On Day 2', () =>{
-    it(`verifyLevelsAreStable function should return false, with unmonotone input`, ()=>{
-        expect(day2.verifyLevelsAreMonotone([1,1,2,2,3])).toBe(false);
-        expect(day2.verifyLevelsAreMonotone([1,2,1,2,1])).toBe(false);
+    it(`verifyLevelsAreMonotone function should return false, with unmonotone input`, ()=>{
+        expect(day2.verifyLevelsAreMonotone([1,1,2,2,3,3,4,4])).toBe(false);
+        expect(day2.verifyLevelsAreMonotone([1,3,2,4,5])).toBe(false);
         expect(day2.verifyLevelsAreMonotone([3,2,1,2,3])).toBe(false);
+        expect(day2.verifyLevelsAreMonotone([8,6,4,4,1])).toBe(false);
+    })
+});
+
+describe('On Day 2', () => {
+    it(`verifyLevelsAreStable function should return true with stable input`, ()=>{
+        expect(day2.verifyLevelsAreStable([7,6,4,2,1])).toBe(true);
+        expect(day2.verifyLevelsAreStable([1,3,6,7,9])).toBe(true);
+    })
+})
+
+describe('On Day 2', () => {
+    it(`verifyLevelsAreStable function should return false with unstable input`, ()=>{
+        expect(day2.verifyLevelsAreStable([1,2,7,8,9])).toBe(false);
+        expect(day2.verifyLevelsAreStable([9,7,6,2,1])).toBe(false);
+        expect(day2.verifyLevelsAreStable([12,11,9,9,6,5,4,3,2,1])).toBe(false);
+        expect(day2.verifyLevelsAreStable([11,9,8,6,5,4,3,2,2])).toBe(false);
     })
 })
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -13,12 +13,12 @@ class Day2 extends Day {
     }
     
     solveForPartOne(input: string): string {
-        
         let safe_reports = 0;
         let reports = input.split("\n");
         reports.forEach(report => {
             let levels = report.split(" ").map(Number);
-            if (this.verifyLevelsAreMonotone(levels))
+            if (this.verifyLevelsAreMonotone(levels) &&
+                this.verifyLevelsAreStable(levels))
                 safe_reports += 1;
         });
         
@@ -29,6 +29,7 @@ class Day2 extends Day {
         return input;
     }
 
+    // returns true, if levels only increase/decrease.
     verifyLevelsAreMonotone(input: Array<number>): boolean {
         let is_monotone = true; // is the function always increasing/decreasing
         let is_increasing = true; // true means increasing, false means decreasing
@@ -56,28 +57,15 @@ class Day2 extends Day {
         return is_monotone;
     }
 
-    // return true if levels only increase or decrese
+    // verify, that levels always change at least by 1 or 3
     verifyLevelsAreStable(input: Array<number>): boolean {
         let is_safe = true;
-        for (let i = 1; i < input.length; i++) {
-            // rule 1: Levels must decrease or increase.
-            // rule 2: adjacent levels must differ by 1
-            // rule 3: adjacent levels must differ by 3 or less.
-            
+        for (let i = 0; i <= input.length-1; i++) {
             // If absolute value is > 3 or values are same = Unsafe test
             if (Math.abs(input[i-1] - input[i]) >= 4 || input[i-1] - input[i] == 0)
             {
                 is_safe = false;
-            }
-            // if previous and next values are both greater = Unsafe test
-            if (input.indexOf(i+1) != -1 && input[i-1] > input[i] && input[i+1] > input[i])
-            {
-                is_safe = false;
-            }
-            // if previous and next values are both lesser = Unsafe test
-            if (input.indexOf(i+1) != -1 && input[i-1] < input[i] && input[i+1] < input[i])
-            {
-                is_safe = false;
+                break;
             }
         }
         return is_safe;
