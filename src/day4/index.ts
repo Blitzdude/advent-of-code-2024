0d1692acf1a8689595abb890606ce19fa0a73d73
Blitzdude <25263772+Blitzdude@users.noreply.github.com>
2025-05-31 16:26:48 +0300

    Added day 4 files and input.

---
+++ src/day4/index.ts
@@ -0,0 +1,22 @@
+import { Day } from "../day";
+
+class Day4 extends Day {
+
+    constructor(){
+        super(4);
+    }
+
+    solveForSample(input: string): string {
+        return input;
+    }
+    
+    solveForPartOne(input: string): string {
+        return input;
+    }
+
+    solveForPartTwo(input: string): string {
+        return input;
+    }
+}
+
+export default new Day4;
